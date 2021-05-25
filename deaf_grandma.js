35484b8ed1c374bd745067ae1b3fe7b549939257
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-25 10:42:11 -0400

    Deaf Grandma

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,26 @@
 function deafGrandma() {
-
+  console.log("HEY KID!");
+  var goodbye=false;
+  while(true){
+    var input=prompt("\n");
+    if(input!=input.toUpperCase()){
+      console.log("SPEAK UP, KID!")
+    }
+    else if(input==""){
+      console.log("WHAT?!")
+    }
+    else if(input=="GOODBYE!"){
+      if(goodbye){
+        console.log("LATER, SKATER!")
+        break;
+      }
+      console.log("LEAVING SO SOON?")
+      goodbye=true;
+    }
+    else{
+      console.log("NO, NOT SINCE 1946!")
+    }
+  }
 }
 
 deafGrandma();
