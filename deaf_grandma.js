d7396c9f6b30f6b68df8836799d07787998adc4b
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-25 13:03:40 -0400

    Deaf Grandma

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,25 +1,28 @@
 function deafGrandma() {
   console.log("HEY KID!");
-  var goodbye=false;
-  while(true){
-    var input=prompt("\n");
-    if(input!=input.toUpperCase()){
-      console.log("SPEAK UP, KID!")
-    }
-    else if(input==""){
-      console.log("WHAT?!")
-    }
-    else if(input=="GOODBYE!"){
-      if(goodbye){
-        console.log("LATER, SKATER!")
-        break;
+  var goodbye=0;
+  prompt.start();
+  while(goodbye<2){
+    prompt.get(['input'], function(err, result) {
+      const input = result.input;
+      if(input!=input.toUpperCase()){
+        console.log("SPEAK UP, KID!")
       }
-      console.log("LEAVING SO SOON?")
-      goodbye=true;
-    }
-    else{
-      console.log("NO, NOT SINCE 1946!")
-    }
+      else if(input==""){
+        console.log("WHAT?!")
+      }
+      else if(input=="GOODBYE!"){
+        if(goodbye){
+          goodbye++;
+          console.log("LATER, SKATER!")
+        }
+        console.log("LEAVING SO SOON?")
+        goodbye++;
+      }
+      else{
+        console.log("NO, NOT SINCE 1946!")
+      }
+    })
   }
 }
 
