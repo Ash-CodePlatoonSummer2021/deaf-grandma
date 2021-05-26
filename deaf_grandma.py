206ebf77207e959a121458258df4e2cd646798b9
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-26 15:21:32 -0400

    python version

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,19 @@
 def deaf_grandma():
-
-
+    print("HEY KID!");
+    goodbye=0;
+    while(goodbye<2):
+        inp = input("")
+        if(inp!=inp.upper()):
+            print("SPEAK UP, KID!")
+        elif(inp==""):
+            print("WHAT?!")
+        elif(inp=="GOODBYE!"):
+            if(goodbye):
+                goodbye+=1
+                print("LATER, SKATER!")
+            print("LEAVING SO SOON?")
+            goodbye+=1
+        else:
+            print("NO, NOT SINCE 1946!")
 
 deaf_grandma()
